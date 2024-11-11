954f056866c2a220b41cdb2f5e12ee30474bd201
910jmiguel <910jmiguel@gmail.com>
2024-11-11 01:19:54 -0500

    Third Commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,7 +12,7 @@ const Nav = () => {
                     alt="Logo"
                     width={130}
                     height={29}
-                />
+                /> 
             </a>
             <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
               {navLinks.map((item) => (
@@ -26,11 +26,12 @@ const Nav = () => {
                 </li>
               ))}
             </ul>
+            <div className="hidden max-lg:block">
+              <img src={hamburger} alt="Hamburger" width={25} height={25}/>
+            </div>
         </nav>
     </header>
   )
 }
-
-//1:14:21 of video
 
 export default Nav
